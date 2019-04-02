3fb1fa67d96d0d9bc44f2dfb6954fe407fe93e9a
Syed abdul rahim <syedabdulrahim4@gmail.com>
2019-04-02 13:38:49 +0530

    Change

--- MinandmaxDigits.java
+++ MinandmaxDigits.java
@@ -1,41 +1,37 @@
-package tenMarks;
+import java.util.*;
+public class MinMax {
 
-import java.util.Scanner;
-
-public class MinandmaxDigits {
-
-	public static void main(String [] args) {
-
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
 		
-	int temp=0;
-	int max=0;
-	int least=0;
-	Scanner sc=new Scanner(System.in);
-	int num=sc.nextInt();
-	int num2=num;
-	while(num!=0) {
-	
-		temp=num%10;
-		if(max<temp) {
-			max=temp;
+		int num=sc.nextInt();
+		int temp;
+		int max=0;
+		int num2=num;
+		int min=9;
+		while(num!=0) {
+			
+			temp=num%10;
+			if(max<temp) {
+				max=temp;
+			}
+			num=num/10;
 			
 		}
-		num=num/10;
-	
-	}
-	while(num2!=0) {
 		
-		temp=num2%10;
-		if(least>temp) {
-			least=temp;
+		
+		while(num2!=0) {
+			temp=num2%10;
+			if(min>temp) {
+				min=temp;
+		
 			
+			}
+			num2=num2/10;
 		}
-		num2=num2/10;
-	
-	}
-	
-	sc.close();
+
 	System.out.println(max);
-	System.out.println(least);
+	System.out.println(min);
 	}
+
 }
